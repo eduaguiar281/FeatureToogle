1b988fea482a3244cc1342882b548fe55d4414a3
Eduardo Aguiar <eduaguiar281@hotmail.com>
2021-05-28 20:21:14 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FeatureToogle.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
